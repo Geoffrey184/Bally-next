5555d59b09d9dd5c2d11c1a1e4d1bc0f5d5fe809
Geoffrey184 <geoffrey.noel08@gmail.com>
2025-01-07 15:27:51 +0100

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Bally-next',
+  assetPrefix: isProd ? '/Bally-next/' : '',
+  trailingSlash: true, // Assure des chemins avec des slashes finaux pour éviter les erreurs 404
 };
-
+  
 export default nextConfig;
